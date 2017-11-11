97864ea6f00fb1a9bc1e0242674e77375a853907
Wannamiu <58030218@kmitl.ac.th>
2017-11-11 20:58:02 +0700

    commited by wannisa

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -24,23 +24,15 @@ namespace GDIPlus_1
                 Graphics g = e.Graphics;
                 Bitmap bmp = new Bitmap("E:\\BGP.PNG");
                 this.SetClientSizeCore(bmp.Width, bmp.Height);
-                Rectangle topleft = new Rectangle(0, 0, bmp.Width /2, bmp.Height/2) ;
-                Rectangle topRight = new Rectangle(bmp.Width/2, 0, bmp.Width/2, bmp.Height/2);
-                Rectangle bottomleft = new Rectangle(0, bmp.Height / 2, bmp.Width, bmp.Height/2);
-                Rectangle bottomRight = new Rectangle(bmp.Width/2, bmp.Height / 2, bmp.Width/2, bmp.Height/2);
-
-
-                bmp.RotateFlip(RotateFlipType.RotateNoneFlipNone);
-                e.Graphics.DrawImage(bmp, topleft);
-
-            bmp.RotateFlip(RotateFlipType.RotateNoneFlipX);
-            e.Graphics.DrawImage(bmp, topRight);
-
-            bmp.RotateFlip(RotateFlipType.Rotate180FlipNone);
-            e.Graphics.DrawImage(bmp, bottomleft);
-
-            bmp.RotateFlip(RotateFlipType.Rotate180FlipY);
-            e.Graphics.DrawImage(bmp, bottomRight);
+                Rectangle destrect = new Rectangle(0, 0, bmp.Width, bmp.Height) ;
+                Brush myBrush = new SolidBrush(Color.Coral);
+            g.DrawImage(bmp, destrect);
+            g.DrawString("Hello World",
+                new Font("Verdana", 30, FontStyle.Bold),//Font,size,style
+                myBrush,
+                0, // x position to display font
+                0); // y position to display font
+            
 
             g.Dispose();
             
