a4c1a6d48d14cc1b5ba802f9e0eabce115668f74
Wannamiu <58030218@kmitl.ac.th>
2017-11-11 16:10:27 +0700

    commited by wannisa

--- GDIPlus_1/GDIPlus_1/Form1.cs
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -21,14 +21,16 @@ namespace GDIPlus_1
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
-            HatchBrush mybrush = new HatchBrush(Hatchstyle.DrakVertical,
-                Color.White, Color.Violet);
-            Pen mypen = new Pen(mybrush, 5);
-            g.DrawEllipse(mypen, 10, 10, 200, 200);
-            mybrush.Dispose();
-            mypen.Dispose(); 
+            Pen Blackpen = new Pen(Color.Black, 3);
 
-           
+            Point point1 = new Point(100, 100);
+            Point point2 = new Point(200, 200);
+
+            e.Graphics.DrawLine(Blackpen, point1, point2);
+            Blackpen.Dispose(); 
+
+
+
         }
     }
 
