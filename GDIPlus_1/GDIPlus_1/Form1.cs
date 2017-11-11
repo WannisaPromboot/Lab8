2d7a55e15de9fcbaa1092f650ce07e8bca55046c
Wannamiu <58030218@kmitl.ac.th>
2017-11-11 15:38:19 +0700

    commited by wannisa

---
+++ GDIPlus_1/GDIPlus_1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GDIPlus_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen bluepen = new Pen(Color.Blue, 1);
+            g.DrawRectangle(bluepen, 10, 10, 100, 100);
+            bluepen.Dispose();
+
+            Pen somepen = new Pen(Color.FromArgb(255,120,200));
+            g.DrawEllipse (somepen , 20, 20, 200, 200);
+            somepen.Dispose();
+        }
+    }
+}
